c22bf728243028a105ee255274faea83ed21f700
xcwang <1366993017@qq.com>
2023-12-11 11:18:49 +0800

    完成 pr body 补充

--- src/pr.rs
+++ src/pr.rs
@@ -1,7 +1,7 @@
-use crate::config::Config;
+use crate::config::{get_current_path, Config};
 use crate::flow::GitCommand;
 use crate::get_target_issue;
-use crate::view::{ViewHandler, View};
+use crate::view::{View, ViewHandler};
 pub struct Pr {
     config: Config,
     all_branch: Vec<String>,
@@ -12,6 +12,7 @@ pub struct Pr {
 use log::{debug, error, info, warn};
 
 impl GitCommand for Pr {}
+const PR_MD_TEMPLATE: &str = ".github/PULL_REQUEST_TEMPLATE.md";
 
 impl Pr {
     pub fn new(c: String, config: &Config, force: bool, chooise: String) -> Self {
@@ -55,6 +56,7 @@ impl Pr {
 
 pub trait PrCommand {
     fn pr(&self);
+    fn get_pr_body(&self) -> String;
 }
 
 impl PrCommand for Pr {
@@ -77,15 +79,51 @@ impl PrCommand for Pr {
         debug!("push result: {:#?}", result);
         println!("{}", std::string::String::from_utf8(result).unwrap());
         // 推送后执行 gh 命令 创建 pr
+        let git_path = self.get_pr_body();
+        // 不同的操作系统的路径分隔符不一样，这里判断下编译的操作系统， 然后拼接
+        let format_path = std::path::Path::new(&git_path);
+        let pr_md_path = format_path.join(PR_MD_TEMPLATE);
+        debug!("pr_md_path: {:?}", pr_md_path);
+        // 判断文件是否存在，如果存在就读取文件内容
+        let pr_body_path = if pr_md_path.exists() {
+            pr_md_path
+        } else {
+            std::path::Path::new(PR_MD_TEMPLATE).to_path_buf()
+        };
+
         let pr_cmd = format!(
-            "gh pr create -f -H {} -B {} --body 'asdfasf'",
-            current_branch, self.biggerst_branch_name
+            "gh pr create -f -H {} -B {} -F {}",
+            current_branch, self.biggerst_branch_name, pr_body_path.display()
         );
-        debug!("execute pr_cmd: {}", pr_cmd);
+        info!("execute pr_cmd: {}", pr_cmd);
         let pr_result = self.run_command_and_check_code(&pr_cmd);
         println!("{}", std::string::String::from_utf8(pr_result).unwrap());
         let view = ViewHandler::new("pr".to_string());
         view.run_command();
         std::process::exit(0);
+    }
+
+    fn get_pr_body(&self) -> String {
+        let git_path = get_current_path();
+        match git_path {
+            Ok(path) => path.to_string(),
+            Err(e) => {
+                error!("get git path error: {}", e);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_github_md () {
+        let current_git_path = ".github/PULL_REQUEST_TEMPLATE.md";
+        // 判断文件是否存在，如果存在就读取文件内容
+        let pr_body = std::fs::read_to_string(current_git_path).unwrap();
+        println!("{}", pr_body);
     }
 }
